c907ca03a53e5ad5f175053bab136c41a8a1dfc6
Documenter.jl <documenter@juliadocs.github.io>
2022-07-02 23:16:22 +0000

    build based on d677c0e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Thurayya.jl","page":"Thurayya.jl","title":"Thurayya.jl","text":"","category":"section"},{"location":"","page":"Thurayya.jl","title":"Thurayya.jl","text":"Documentation for Thurayya.jl","category":"page"}]
+}
